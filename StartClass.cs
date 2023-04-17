31dc93379495c96ae14c926488c99546e5ca336c
opilane <opilane@HP-CZC2349HTP>
2023-04-17 12:15:59 +0300

    Add project files.

---
+++ StartClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listid_ja_sõnastikud
+{
+    public class StartClass
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> eesti = new Dictionary<string,string>(5);
+        }
+    }
+}
